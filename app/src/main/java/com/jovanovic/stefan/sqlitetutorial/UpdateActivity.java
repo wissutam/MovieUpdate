64d5fc68acb1cfb99167b47730d8d96bcb50b893
wissutam <wissuta97@gmail.com>
2020-11-18 19:07:17 +0700

    Initial commit

---
+++ app/src/main/java/com/jovanovic/stefan/sqlitetutorial/UpdateActivity.java
@@ -0,0 +1,105 @@
+package com.jovanovic.stefan.sqlitetutorial;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class UpdateActivity extends AppCompatActivity {
+
+    EditText name_input, type_input, runtime_input, plot_input;
+    Button update_button, delete_button;
+
+    String id, name, type, runtime, plot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+
+        name_input = findViewById(R.id.movie_input2);
+        type_input = findViewById(R.id.type_input2);
+        runtime_input = findViewById(R.id.runtime_input2);
+        plot_input = findViewById(R.id.plot_input2);
+        update_button = findViewById(R.id.update_button);
+        delete_button = findViewById(R.id.delete_button);
+
+        //First we call this
+        getAndSetIntentData();
+
+        //Set actionbar title after getAndSetIntentData method
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setTitle(name);
+        }
+
+        update_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //And only then we call this
+                MyDatabaseHelper myDB = new MyDatabaseHelper(UpdateActivity.this);
+                name = name_input.getText().toString().trim();
+                type = type_input.getText().toString().trim();
+                runtime = runtime_input.getText().toString().trim();
+                plot = plot_input.getText().toString().trim();
+                myDB.updateData(id, name, type, runtime, plot);
+            }
+        });
+        delete_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                confirmDialog();
+            }
+        });
+
+    }
+
+    void getAndSetIntentData(){
+        if(getIntent().hasExtra("id") && getIntent().hasExtra("name") &&
+                getIntent().hasExtra("type") && getIntent().hasExtra("runtime")&& getIntent().hasExtra("plot")){
+            //Getting Data from Intent
+            id = getIntent().getStringExtra("id");
+            name = getIntent().getStringExtra("name");
+            type = getIntent().getStringExtra("type");
+            runtime = getIntent().getStringExtra("runtime");
+            plot = getIntent().getStringExtra("plot");
+
+            //Setting Intent Data
+            name_input.setText(name);
+            type_input.setText(type);
+            runtime_input.setText(runtime);
+            plot_input.setText(plot);
+            Log.d("stev", name +" "+ type +" "+ runtime+" "+ plot);
+        }else{
+            Toast.makeText(this, "No data.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void confirmDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete " + name + " ?");
+        builder.setMessage("Are you sure you want to delete " + name + " ?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(UpdateActivity.this);
+                myDB.deleteOneRow(id);
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.create().show();
+    }
+}
